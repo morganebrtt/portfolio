f3b3ed76164277d926d5220d37db5672550cfdd6
Morgane Broutet <morgane.broutet@gmail.com>
2020-03-01 21:28:31 +0100

    add header structure & style

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -3,14 +3,30 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav id="top-nav">
-            <ul>
-                <li><a href="#">home</a></li>
-                <li><a href="#">projects</a></li>
-                <li><a href="#">about</a></li>
-                <li><a href="#">contact</a></li>
-            </ul>
-        </nav>
+        <>
+            <nav id="top-nav" role="navigation">
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">projects</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">contact</a></li>
+                </ul>
+            </nav>
+            <nav id="side-nav" role="navigation">
+                <div id="menuToggle">
+                    <input type="checkbox" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <ul id="menu">
+                        <a href="#"><li>home</li></a>
+                        <a href="#"><li>projects</li></a>
+                        <a href="#"><li>about</li></a>
+                        <a href="#"><li>contact</li></a>
+                    </ul>
+                </div>
+            </nav>
+        </>
     );
 };
 
